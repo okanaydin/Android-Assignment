b5e274b212446978135b7fff8c0917333e5c4126
okanaydin <okan+github@citizenme.com>
2021-04-16 00:38:15 +0300

    update version of the libraries for Gradle, AndroidX

--- app/src/main/java/app/storytel/candidate/com/ScrollingActivity.java
+++ app/src/main/java/app/storytel/candidate/com/ScrollingActivity.java
@@ -2,10 +2,10 @@ package app.storytel.candidate.com;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
