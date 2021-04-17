3c616be5af50073138a04acda2920dc53c075e65
okanaydin <okan+github@citizenme.com>
2021-04-17 03:52:44 +0300

    create remote data module

---
+++ app/src/main/java/app/storytel/candidate/com/data/di/RemoteDataModule.kt
@@ -0,0 +1,41 @@
+package app.storytel.candidate.com.data.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RemoteDataModule {
+
+    private const val BASE_API_URL = "https://jsonplaceholder.typicode.com/"
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BASIC
+        }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(BASE_API_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+}
