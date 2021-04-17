68fd9d5f845c09ecf3a92f748613abb303d97bb9
okanaydin <okan+github@citizenme.com>
2021-04-18 02:34:03 +0300

    implement data-source for comments

--- app/src/main/java/app/storytel/candidate/com/data/remote/datasource/posts/PostDataSource.kt
+++ app/src/main/java/app/storytel/candidate/com/data/remote/datasource/posts/PostDataSource.kt
@@ -1,8 +1,11 @@
 package app.storytel.candidate.com.data.remote.datasource.posts
 
+import app.storytel.candidate.com.data.remote.datasource.model.CommentModel
 import app.storytel.candidate.com.data.remote.datasource.model.PostModel
 
 interface PostDataSource {
 
     suspend fun getPosts(): List<PostModel>
+
+    suspend fun getComments(id: Int): List<CommentModel>
 }
