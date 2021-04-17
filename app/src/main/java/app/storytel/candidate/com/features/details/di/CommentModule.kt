7c5687095a07ceeaa1e56002cf3b8be412326664
okanaydin <okan+github@citizenme.com>
2021-04-18 02:42:31 +0300

    create comment module

---
+++ app/src/main/java/app/storytel/candidate/com/features/details/di/CommentModule.kt
@@ -0,0 +1,24 @@
+package app.storytel.candidate.com.features.details.di
+
+import app.storytel.candidate.com.data.remote.api.PhotoService
+import app.storytel.candidate.com.data.remote.api.PostService
+import app.storytel.candidate.com.data.remote.datasource.photos.PhotoDataSource
+import app.storytel.candidate.com.data.remote.datasource.photos.PhotoDataSourceImp
+import app.storytel.candidate.com.data.remote.datasource.posts.PostDataSource
+import app.storytel.candidate.com.data.remote.datasource.posts.PostDataSourceImp
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CommentModule {
+
+    @Provides
+    @Singleton
+    fun providePostDataSource(postService: PostService): PostDataSource {
+        return PostDataSourceImp(postService)
+    }
+}
